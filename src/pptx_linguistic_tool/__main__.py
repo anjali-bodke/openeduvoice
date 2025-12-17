95f06bace59165477af591148d1751848ab0da1f
Anjali Bodke <anjalibodke2022@gmail.com>
2025-12-17 22:29:13 +0100

    chore: rename package to pptx_linguistic_tool and add gui

---
+++ src/pptx_linguistic_tool/__main__.py
@@ -0,0 +1,10 @@
+import tkinter as tk
+from pptx_linguistic_tool.app.main_window import App
+
+def main():
+    root = tk.Tk()
+    App(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
